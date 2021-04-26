b97d3a9bea10d8e8a0367c2a68daa77e9e28c617
zakski <zak.szadowski+git@gmail.com>
2021-04-26 14:25:59 +0100

    Update Java file headers to match license

--- src/main/java/com/szadowsz/datamuse/DatamuseQuery.java
+++ src/main/java/com/szadowsz/datamuse/DatamuseQuery.java
@@ -1,10 +1,19 @@
-/*
-* Copyright © 2015 S.J. Blair <https://github.com/sjblair>
-* This work is free. You can redistribute it and/or modify it under the
-* terms of the Do What The Fuck You Want To Public License, Version 2,
-* as published by Sam Hocevar. See the COPYING file for more details.
-*/
-
+/**
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
 package com.szadowsz.datamuse;
 
 import java.io.BufferedReader;
